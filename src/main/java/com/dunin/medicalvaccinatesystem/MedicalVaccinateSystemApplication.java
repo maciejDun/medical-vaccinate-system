ce0073ee0fd6174a74791cdf3073fedfd96975d2
maciejDun <78140796+maciejDun@users.noreply.github.com>
2021-05-28 15:03:14 +0200

    Spring initial project structure build

---
+++ src/main/java/com/dunin/medicalvaccinatesystem/MedicalVaccinateSystemApplication.java
@@ -0,0 +1,13 @@
+package com.dunin.medicalvaccinatesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalVaccinateSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalVaccinateSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dunin/medicalvaccinatesystem/MedicalVaccinateSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dunin.medicalvaccinatesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalVaccinateSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
