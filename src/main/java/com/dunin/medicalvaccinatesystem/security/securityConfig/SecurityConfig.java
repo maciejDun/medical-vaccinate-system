012ffe66bf649686437f7841d0a0bc3ca81cd1da
maciejDun <78140796+maciejDun@users.noreply.github.com>
2021-06-04 10:47:01 +0200

    Code refactoring

--- src/main/java/com/dunin/medicalvaccinatesystem/security/securityConfig/SecurityConfig.java
+++ src/main/java/com/dunin/medicalvaccinatesystem/security/securityConfig/SecurityConfig.java
@@ -6,13 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -31,17 +24,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .userInfoEndpoint()
             .userService(customOAuth2UserService)
             .and()
-            .successHandler(new AuthenticationSuccessHandler() {
-                @Override
-                public void onAuthenticationSuccess(HttpServletRequest httpServletRequest,
-                                                    HttpServletResponse httpServletResponse,
-                                                    Authentication authentication)
-                        throws IOException, ServletException {
+            .successHandler((httpServletRequest, httpServletResponse, authentication) -> {
 
-                    customOAuth2UserService.processOAuthPostLogin();
+                customOAuth2UserService.processOAuthPostLogin();
 
-                    httpServletResponse.sendRedirect("/google");
-                }
+                httpServletResponse.sendRedirect("/google");
             });
     }
 
